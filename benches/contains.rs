ea4247be7db1d414993471f69fdee84132301a2d
Christian Schleifer <chr.schleifer@gmail.com>
2024-08-18 14:46:56 +0200

    Pull variable access out of benchmark loop

--- benches/contains.rs
+++ benches/contains.rs
@@ -7,34 +7,38 @@ fn contains(c: &mut Criterion) {
     let datasets = &DATASETS;
 
     for dataset in datasets.iter() {
-        let croaring = dataset.croaring_bitmaps();
-        let croaring: Vec<_> = croaring.iter().collect();
+        let croaring_bms = dataset.croaring_bitmaps();
+        let croaring_bms: Vec<_> = croaring_bms.iter().collect();
+
+        let first_quartile_value = dataset.statistics().first_quartile().value();
+        let second_quartile_value = dataset.statistics().second_quartile().value();
+        let third_quartile_value = dataset.statistics().third_quartile().value();
 
         group.bench_with_input(
             BenchmarkId::new("croaring", dataset.name()),
-            &(croaring, dataset.statistics()),
-            |bencher, (bitmaps, statistics)| {
+            &(croaring_bms, (first_quartile_value, second_quartile_value, third_quartile_value)),
+            |bencher, (bitmaps, (first_quartile_value, second_quartile_value, third_quartile_value))| {
                 bencher.iter(|| {
                     for bitmap in bitmaps {
-                        let _first = bitmap.contains(statistics.first_quartile().value());
-                        let _second = bitmap.contains(statistics.second_quartile().value());
-                        let _third = bitmap.contains(statistics.third_quartile().value());
+                        let _first = bitmap.contains(*first_quartile_value);
+                        let _second = bitmap.contains(*second_quartile_value);
+                        let _third = bitmap.contains(*third_quartile_value);
                     }
                 })
             },
         );
 
-        let roaring = dataset.roaring_rs_bitmaps();
+        let roaring_rs_bms = dataset.roaring_rs_bitmaps();
 
         group.bench_with_input(
             BenchmarkId::new("roaring-rs", dataset.name()),
-            &(roaring, dataset.statistics()),
-            |bencher, (bitmaps, statistics)| {
+            &(roaring_rs_bms, (first_quartile_value, second_quartile_value, third_quartile_value)),
+            |bencher, (bitmaps, (first_quartile_value, second_quartile_value, third_quartile_value))| {
                 bencher.iter(|| {
                     for bitmap in *bitmaps {
-                        let _first = bitmap.contains(statistics.first_quartile().value());
-                        let _second = bitmap.contains(statistics.second_quartile().value());
-                        let _third = bitmap.contains(statistics.third_quartile().value());
+                        let _first = bitmap.contains(*first_quartile_value);
+                        let _second = bitmap.contains(*second_quartile_value);
+                        let _third = bitmap.contains(*third_quartile_value);
                     }
                 })
             },
