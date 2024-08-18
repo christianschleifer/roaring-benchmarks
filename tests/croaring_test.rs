c4224c680faa4963b76d01289224f0b25585604a
Christian Schleifer <chr.schleifer@gmail.com>
2024-08-18 14:22:02 +0200

    Add correctness tests for croaring and roaring-rs

---
+++ tests/croaring_test.rs
@@ -0,0 +1,37 @@
+use roaring_benchmarks::DATASETS;
+
+#[test]
+fn it_computes_expected_results() {
+    let datasets = &DATASETS;
+
+    for dataset in datasets.iter() {
+        let first_quartile = dataset.statistics().first_quartile().value();
+        let second_quartile = dataset.statistics().second_quartile().value();
+        let third_quartile = dataset.statistics().third_quartile().value();
+
+        let mut first_quartile_results = Vec::with_capacity(dataset.raw_data().len());
+        let mut second_quartile_results = Vec::with_capacity(dataset.raw_data().len());
+        let mut third_quartile_results = Vec::with_capacity(dataset.raw_data().len());
+
+        let bms = dataset.croaring_bitmaps();
+
+        for bm in bms {
+            first_quartile_results.push(bm.contains(first_quartile));
+            second_quartile_results.push(bm.contains(second_quartile));
+            third_quartile_results.push(bm.contains(third_quartile));
+        }
+
+        assert_eq!(
+            dataset.expected_results().contains_first_quartile_value(),
+            first_quartile_results
+        );
+        assert_eq!(
+            dataset.expected_results().contains_second_quartile_value(),
+            second_quartile_results
+        );
+        assert_eq!(
+            dataset.expected_results().contains_third_quartile_value(),
+            third_quartile_results
+        );
+    }
+}
--- tests/dataset_test.rs
+++ tests/dataset_test.rs
@@ -1,7 +1,7 @@
 use roaring_benchmarks::DATASETS;
 
 #[test]
-fn test() {
+fn it_sets_up_all_datasets() {
     let datasets = &DATASETS;
 
     assert_eq!(datasets.len(), 8)
