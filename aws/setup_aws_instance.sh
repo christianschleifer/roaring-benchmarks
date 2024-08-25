06c91c253c95b43b51eea3cfbcccef4ef47d528b
Christian Schleifer <chr.schleifer@gmail.com>
2024-08-25 09:14:11 +0200

    Add script to set up AWS instance

---
+++ aws/setup_aws_instance.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+# install dependencies
+sudo yum install -y gcc
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# init the benchmark setup
+git submodule update --init --recursive
+
+
