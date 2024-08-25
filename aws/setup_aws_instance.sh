b5055c0dbbf5dbcad4586ce47a333a5b572a7330
Christian Schleifer <chr.schleifer@gmail.com>
2024-08-25 11:26:18 +0200

    Add gnuplot to installs to generate graphs using gnuplot

--- aws/setup_aws_instance.sh
+++ aws/setup_aws_instance.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # install dependencies
-sudo yum install -y gcc
+sudo yum install -y gcc gnuplot
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # init the benchmark setup
